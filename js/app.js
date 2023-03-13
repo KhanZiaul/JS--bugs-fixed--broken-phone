a880ca7f087ced14ffbcd70797a21b2191d3d37b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-14 01:30:49 +0600

    bugs :loadPhoneDetails url fixed

--- js/app.js
+++ js/app.js
@@ -7,8 +7,6 @@ const loadPhones = async(searchText, dataLimit) =>{
 
 const displayPhones = (phones, dataLimit) =>{
 
-    console.log(phones)
-
     const phonesContainer = document.getElementById('phones-container');
 
     phonesContainer.textContent = '';
@@ -36,16 +34,14 @@ const displayPhones = (phones, dataLimit) =>{
     // display all phones
     phones.forEach(phone =>{
 
-        console.log(phone)
-
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
         <div class="card p-4">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${phone.brand}</h5>
+                <p class="card-text">${phone.phone_name}</p>
                 <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                 
             </div>
@@ -98,10 +94,11 @@ document.getElementById('btn-show-all').addEventListener('click', function(){
 })
 
 const loadPhoneDetails = async id =>{
-    const url =`www.openapi.programming-hero.com/api/phone/${id}`;
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhoneDetails(data.data);
+    console.log(data)
 }
 
 const displayPhoneDetails = phone =>{
